b8b1888cc85c64f456c7788bede208aa59208565
justinlees <121879669+justinlees@users.noreply.github.com>
2025-10-24 23:42:28 +0530

    BugFix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,13 @@
 import "./App.css";
 import { BrowserRouter, Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 function App() {
   return (
     <>
       <BrowserRouter>
-        <Router>
-          <Routes>
-            <Route />
-          </Routes>
-        </Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </BrowserRouter>
     </>
   );
