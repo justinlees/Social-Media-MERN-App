4fec4c744768242ad3019ee6a5c922a2156d7abd
justinlees <121879669+justinlees@users.noreply.github.com>
2025-11-01 00:49:53 +0530

    fixedAllPostsFetching

--- frontend/src/Components/AllPosts.jsx
+++ frontend/src/Components/AllPosts.jsx
@@ -1,38 +1,98 @@
-export default function AllPosts() {
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+export default function AllPosts({ posts }) {
+  const params = useParams();
+  const [isValidLike, setIsValidLike] = useState(false);
+  const [allPosts, setAllPosts] = useState([]);
+  const handleSubmit = async (postId) => {
+    const formData = {
+      _id: postId,
+      likedBy: params.userId,
+    };
+    try {
+      const response = await fetch(
+        `http://localhost:5000/${params.userId}/homePage/userProfile/likePost`,
+        {
+          method: "POST",
+          headers: {
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        }
+      );
+
+      if (response.status === 200) {
+        window.location.reload();
+      }
+    } catch (error) {
+      console.error("Server Error", error);
+    }
+  };
+
+  useEffect(() => {
+    try {
+      const fetchData = async () => {
+        const response = await fetch(
+          `http://localhost:5000/${params.userId}/homePage/allPosts`
+        );
+
+        if (response.status === 200) {
+          const data = await response.json();
+          setAllPosts(data.posts);
+        }
+      };
+      fetchData();
+    } catch (error) {
+      console.log("Server Error", error);
+    }
+  }, [params.userId]);
+
   return (
     <div className="allPosts">
-      <section className="card">
-        <div className="cardHeader">
-          <div className="cardUser">
-            <img />
-            <p>UserName</p>
+      {allPosts?.map((post) => (
+        <section className="card" key={post?._id}>
+          <div className="cardHeader">
+            <div className="cardUser">
+              <img />
+              <p>{post?.userName}</p>
+            </div>
+            <span>::</span>
           </div>
-          <span>::</span>
-        </div>
-        <div className="cardPost">
-          <img src="https://images.hindustantimes.com/rf/image_size_960x540/HT/p2/2018/05/07/Pictures/fbl-esp-liga-barcelona-real-madrid_e11c3a40-51c4-11e8-aa60-d0af28d4422a.jpg" />
-        </div>
-        <div className="postCaption">
-          <div className="cardReview">
-            <p>
-              <span className="material-symbols-outlined">
-                sentiment_satisfied
-              </span>
-              1,578
-            </p>
-            <p>
-              <span className="material-symbols-outlined">mode_comment</span>325
-            </p>
-            <p>
-              <span className="material-symbols-outlined">send</span>
+          <div className="cardPost">
+            <img src="https://static.skillshare.com/uploads/discussion/tmp/cbe48544.jpg" />
+          </div>
+          <div className="postCaption">
+            <div className="cardReview">
+              <p>
+                <span
+                  className="material-symbols-outlined"
+                  onClick={() => {
+                    if (post?.userId !== params.userId) {
+                      setIsValidLike(!isValidLike);
+                      if (isValidLike) handleSubmit(post?._id);
+                    }
+                  }}
+                >
+                  sentiment_satisfied
+                </span>
+                {post?.likes}
+              </p>
+              <p>
+                <span className="material-symbols-outlined">mode_comment</span>
+                {post?.comments}
+              </p>
+              <p>
+                <span className="material-symbols-outlined">send</span>
+              </p>
+            </div>
+            <p className="caption">
+              <span>{post?.userName}</span>
+              {post?.postCaption}
             </p>
           </div>
-          <p className="caption">
-            <span>UserName</span>
-            The Caption for the image... readMore
-          </p>
-        </div>
-      </section>
+        </section>
+      ))}
     </div>
   );
 }
