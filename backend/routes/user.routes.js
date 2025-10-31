23f7d3b5eab6b70aeda96122fefc986a8d8b89a1
justinlees <121879669+justinlees@users.noreply.github.com>
2025-11-01 00:53:00 +0530

    addedAllPostFetchandLikePostAPI

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -4,10 +4,14 @@ const {
   getUserDetails,
   getUserPosts,
   userPostCreation,
+  incrementPostLike,
+  getAllPosts,
 } = require("../controllers/user.controller.js");
 
 router.get("/:userId/homePage", getUserDetails);
+router.get("/:userId/homePage/allPosts", getAllPosts);
 router.get("/:userId/homePage/userProfile", getUserPosts);
 router.post("/:userId/homePage/userProfile/postCreation", userPostCreation);
+router.post("/:userId/homePage/userProfile/likePost", incrementPostLike);
 
 module.exports = router;
