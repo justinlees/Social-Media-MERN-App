80e490262c20edb8beb0899c19bfea45bf3dd8bb
justinlees <121879669+justinlees@users.noreply.github.com>
2025-11-01 23:20:39 +0530

    implementedSignInFunctionality

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -1,22 +1,68 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export default function SignIn() {
+  const [loading, setLoading] = useState(false);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const formData = {
+      userName: e.target.userName.value,
+      password: e.target.password.value,
+    };
+    try {
+      const response = await fetch(`http://localhost:5000/account/signIn`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      setLoading(false);
+      if (response.status === 200) {
+        const data = await response.json();
+        window.location = `/${data.user._id}/homePage`;
+      } else if (response.status === 404) {
+        alert("UserName or Password is wrong");
+      } else {
+        alert("Server Error, please try later.");
+      }
+    } catch (error) {
+      console.error("Unable to login", error);
+    }
+  };
   return (
-    <div>
-      <form>
+    <div className="account">
+      <form method="POST" onSubmit={handleSubmit}>
+        <h2>SignIn</h2>
         <fieldset>
-          <label>Enter UserName or Mobile</label>
-          <input type="text" required />
+          <input
+            type="text"
+            name="userName"
+            placeholder="Enter UserName or Mobile"
+            required
+          />
         </fieldset>
         <fieldset>
-          <label>Enter Password</label>
-          <input type="password" required />
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter Password"
+            required
+          />
         </fieldset>
-        <button className="positiveBtn">Login</button>
+        <fieldset>
+          <button className="positiveBtn" type="submit">
+            {loading ? <span className="loader"></span> : "Login"}
+          </button>
+        </fieldset>
+        <fieldset>
+          <Link to={``}>forgotPassword?</Link>
+        </fieldset>
+        <fieldset>
+          <Link>SignIn with Google</Link>
+        </fieldset>
       </form>
-      <section>
-        <Link>SignIn with Google</Link>
-      </section>
     </div>
   );
 }
