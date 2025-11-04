682aed4f87bf24eda01a831ef48faa5fe0ae7f04
justinlees <121879669+justinlees@users.noreply.github.com>
2025-11-04 18:09:43 +0530

    debugAllPostDisplay

--- frontend/src/Components/AllPosts.jsx
+++ frontend/src/Components/AllPosts.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import Post from "./Post";
 
 export default function AllPosts({ posts }) {
   const params = useParams();
@@ -50,49 +51,7 @@ export default function AllPosts({ posts }) {
 
   return (
     <div className="allPosts">
-      {allPosts?.map((post) => (
-        <section className="card" key={post?._id}>
-          <div className="cardHeader">
-            <div className="cardUser">
-              <img />
-              <p>{post?.userName}</p>
-            </div>
-            <span>::</span>
-          </div>
-          <div className="cardPost">
-            <img src="https://static.skillshare.com/uploads/discussion/tmp/cbe48544.jpg" />
-          </div>
-          <div className="postCaption">
-            <div className="cardReview">
-              <p>
-                <span
-                  className="material-symbols-outlined"
-                  onClick={() => {
-                    if (post?.userId !== params.userId) {
-                      setIsValidLike(!isValidLike);
-                      if (isValidLike) handleSubmit(post?._id);
-                    }
-                  }}
-                >
-                  sentiment_satisfied
-                </span>
-                {post?.likes}
-              </p>
-              <p>
-                <span className="material-symbols-outlined">mode_comment</span>
-                {post?.comments}
-              </p>
-              <p>
-                <span className="material-symbols-outlined">send</span>
-              </p>
-            </div>
-            <p className="caption">
-              <span>{post?.userName}</span>
-              {post?.postCaption}
-            </p>
-          </div>
-        </section>
-      ))}
+      <Post posts={allPosts} />
     </div>
   );
 }
