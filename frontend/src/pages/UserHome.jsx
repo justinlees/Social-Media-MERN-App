bcf8dcdb1977d66b90bb035d1aa926b84fca7290
justinlees <121879669+justinlees@users.noreply.github.com>
2025-11-06 18:01:22 +0530

    addedLoader

--- frontend/src/pages/UserHome.jsx
+++ frontend/src/pages/UserHome.jsx
@@ -2,16 +2,19 @@ import { useState, useEffect } from "react";
 import { Link, Outlet, useParams } from "react-router-dom";
 
 export default function UserHome() {
-  const [user, setUser] = useState();
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(false);
   const params = useParams();
   useEffect(() => {
     try {
       const fetchData = async () => {
+        setLoading(true);
         const response = await fetch(
           `http://localhost:5000/${params.userId}/homePage`
         );
         const data = await response.json();
         if (response.status === 200) {
+          setLoading(false);
           setUser(data.user);
         } else {
           window.location.href = "/";
@@ -22,50 +25,60 @@ export default function UserHome() {
       consol.error("Error Occured", error);
     }
   }, [params.userID]);
-  return (
-    <div className="userHomePage">
-      <header className="titleHeader">
-        <h1>ViewShare</h1>
-      </header>
-      <div className="content">
-        <div className="leftSide">
-          <nav className="sideNav">
-            <ul>
-              <li>
-                <Link relative="true" to={`.`}>
-                  <span className="material-symbols-outlined">home</span>
-                </Link>
-              </li>
-              <li>
-                <Link>
-                  <span className="material-symbols-outlined">wand_stars</span>
-                </Link>
-              </li>
-              <li>
-                <Link>
-                  <span className="material-symbols-outlined">search</span>
-                </Link>
-              </li>
-              <li>
-                <Link to={`userProfile`}>
-                  <span className="material-symbols-outlined">
-                    account_circle
-                  </span>
-                </Link>
-              </li>
-              <li>
-                <Link to={`userSettings`}>
-                  <span className="material-symbols-outlined">settings</span>
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
 
-        <div className="outletLayout">
-          <Outlet context={user} />
+  if (loading)
+    return (
+      <div className="loadPage">
+        <span className="mainLoader"></span>
+      </div>
+    );
+  else
+    return (
+      <div className="userHomePage">
+        <header className="titleHeader">
+          <h1>ViewShare</h1>
+        </header>
+        <div className="content">
+          <div className="leftSide">
+            <nav className="sideNav">
+              <ul>
+                <li>
+                  <Link relative="true" to={`.`}>
+                    <span className="material-symbols-outlined">home</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link>
+                    <span className="material-symbols-outlined">
+                      wand_stars
+                    </span>
+                  </Link>
+                </li>
+                <li>
+                  <Link>
+                    <span className="material-symbols-outlined">search</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link to={`userProfile`}>
+                    <span className="material-symbols-outlined">
+                      account_circle
+                    </span>
+                  </Link>
+                </li>
+                <li>
+                  <Link to={`userSettings`}>
+                    <span className="material-symbols-outlined">settings</span>
+                  </Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+
+          <div className="outletLayout">
+            <Outlet context={user} />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
 }
