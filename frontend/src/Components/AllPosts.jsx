1e814ad6fa4f6e62442fb1939ad4be8810e90148
justinlees <121879669+justinlees@users.noreply.github.com>
2025-11-06 18:00:44 +0530

    addedLoader

--- frontend/src/Components/AllPosts.jsx
+++ frontend/src/Components/AllPosts.jsx
@@ -4,16 +4,19 @@ import Post from "./post/Post";
 
 export default function AllPosts({ posts }) {
   const params = useParams();
+  const [loading, setLoading] = useState(false);
   const [allPosts, setAllPosts] = useState([]);
 
   useEffect(() => {
     try {
       const fetchData = async () => {
+        setLoading(true);
         const response = await fetch(
           `http://localhost:5000/${params.userId}/homePage/allPosts`
         );
 
         if (response.status === 200) {
+          setLoading(false);
           const data = await response.json();
           setAllPosts(data.posts);
         }
@@ -24,9 +27,22 @@ export default function AllPosts({ posts }) {
     }
   }, [params.userId]);
 
-  return (
-    <div className="allPosts">
-      <Post posts={allPosts} />
-    </div>
-  );
+  if (loading)
+    return (
+      <div className="loadPage">
+        <span className="mainLoader"></span>
+      </div>
+    );
+  else if (!allPosts.length)
+    return (
+      <div className="userHomePage">
+        <h1>No Posts</h1>
+      </div>
+    );
+  else
+    return (
+      <div className="allPosts">
+        <Post posts={allPosts} />
+      </div>
+    );
 }
