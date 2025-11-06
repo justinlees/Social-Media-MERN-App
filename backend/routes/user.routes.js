29531d81fac0d613fb2ad7dea5671954c086390d
justinlees <121879669+justinlees@users.noreply.github.com>
2025-11-06 23:23:04 +0530

    addedEditProfileRoute

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -8,6 +8,7 @@ const {
   getAllPosts,
   postDeletion,
   userAccountDeletion,
+  editUserDetails,
 } = require("../controllers/user.controller.js");
 
 router.get("/:userId/homePage", getUserDetails);
@@ -15,6 +16,7 @@ router.get("/:userId/homePage/allPosts", getAllPosts);
 router.get("/:userId/homePage/userProfile", getUserPosts);
 router.post("/:userId/homePage/userProfile/postCreation", userPostCreation);
 router.post("/:userId/homePage/userProfile/likePost", incrementPostLike);
+router.patch("/:userId/homePage/userSettings/editProfile", editUserDetails);
 router.delete("/:userId/homePage/userProfile/postDeletion", postDeletion);
 router.delete(
   "/:userId/homePage/userSettings/accountDeletion",
