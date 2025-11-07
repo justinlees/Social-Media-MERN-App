744546da17dc5b1ee36903c968ed009babfab9be
justinlees <121879669+justinlees@users.noreply.github.com>
2025-11-07 19:48:39 +0530

    addedGetandPostCommentRoutes

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -9,13 +9,17 @@ const {
   postDeletion,
   userAccountDeletion,
   editUserDetails,
+  commentPost,
+  getComments,
 } = require("../controllers/user.controller.js");
 
 router.get("/:userId/homePage", getUserDetails);
 router.get("/:userId/homePage/allPosts", getAllPosts);
 router.get("/:userId/homePage/userProfile", getUserPosts);
+router.get("/:userId/homePage/:postId/getComments", getComments);
 router.post("/:userId/homePage/userProfile/postCreation", userPostCreation);
 router.post("/:userId/homePage/userProfile/likePost", incrementPostLike);
+router.post("/:userId/homePage/:postId/commentPost", commentPost);
 router.patch("/:userId/homePage/userSettings/editProfile", editUserDetails);
 router.delete("/:userId/homePage/userProfile/postDeletion", postDeletion);
 router.delete(
